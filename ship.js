89e27cc834e316a598cc2866ed174575d6d81c14
Jake Ashton <jakemaddenashton@gmail.com>
2022-10-27 18:54:39 -0700

    Add and verify remaining ship tests

--- ship.js
+++ ship.js
@@ -1,11 +1,11 @@
 const Ship = (length) => {
   let hitCount = 0;
-  const hit = () => hitCount++;
+  const hit = () => ++hitCount;
   const isSunk = () => {
     if (hitCount === length) return true;
     else return false;
   };
-  return { length, hit, isSunk };
+  return { length, hit, isSunk, hitCount };
 };
 
 export { Ship };
--- ship.test.js
+++ ship.test.js
@@ -1,6 +1,17 @@
 /* eslint-disable no-undef */
 import { Ship } from "./ship.js";
 
-test("adds ship length", () => {
-  expect(Ship(5).length).toBe(5);
+const ship = Ship(2);
+
+test("sets ship length", () => {
+  expect(ship.length).toBe(2);
+});
+
+test("hit method increases hit count", () => {
+  expect(ship.hit()).toBe(1);
+});
+
+test("hit method changes isSunk to true once hits = ship length", () => {
+  ship.hit();
+  expect(ship.isSunk()).toBe(true);
 });
